1bddbefb8f0654c7ae72154065f9d540f2219298
lavsora <ya.karpich@gmail.com>
2022-06-10 19:49:05 +0300

    document initialization

--- script.js
+++ script.js
@@ -7,13 +7,26 @@ const square = document.querySelector('#square');
 const cirle = document.querySelector('#circle');
 const e_btn = document.querySelector('#e_btn');
 
-btn.addEventListener('click', function() {
-    square.style.backgroundColor = `${inputText.value}`;
-});
-
 const visionEBtn = function() {
     e_btn.style.display = 'none';
 };
+
+const visionInputRange = function() {
+    document.querySelector('#range-span').textContent = '50%';
+}
+
+const visionCircle = function() {
+    cirle.style.height = '50%';
+    cirle.style.width = '50%';
+}
+
+visionEBtn();
+visionInputRange();
+visionCircle();
+
+btn.addEventListener('click', function() {
+    square.style.backgroundColor = `${inputText.value}`;
+});
 
 inputRange.addEventListener('input', function() {
     cirle.style.height = `${inputRange.value}%`;
@@ -21,5 +34,3 @@ inputRange.addEventListener('input', function() {
 
     document.querySelector('#range-span').textContent = `${inputRange.value}%`
 });
-
-visionEBtn();
